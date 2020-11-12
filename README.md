b72b7640759a1f4122e0b29b25f536bc0ad47e0a
Ilya Radchenko <knownasilya@gmail.com>
2020-11-12 14:19:53 -0500

    read me

---
+++ README.md
@@ -0,0 +1,6 @@
+Run with `node test` and you should see
+
+```
+Program runtime: 0.741ms
+3
+```
--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const addon = require(".");
 
 (async function () {
+  console.time("Program runtime");
   let result = await addon.add(1, 2);
+  console.timeEnd("Program runtime");
 
   console.log(result);
 })();
